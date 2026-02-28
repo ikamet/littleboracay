d12305352e1e0d95b2e48ebdd03887bf8b01e2b6
Ikamet <85058686+ikamet@users.noreply.github.com>
2026-02-28 22:02:03 +0300

    Change site title and description
    Updated site title and description for the website.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Little Boracay Calatagan Batangas | Sandbar & Tours";
+export const SITE_DESCRIPTION = "Explore Little Boracay in Calatagan, Batangas. Boat tours, floating cottages, crystal waters, white sandbars, and water activities near Calatagan beach.";
